f4ccc7ced728bd7a98b1ee9a33adff3be0d8e181
Alexis Varsava <av11sl@brocku.ca>
2017-06-28 15:25:30 -0400

    Implements incrementBest, codifies row names
    For automatically incrementing the best streak. This will get called
    when currentStreak > bestStreak.
    
    Introduces R_CURRENT and R_BEST so I don't have to worry about typos
    with the only two rows there will ever be in this database.

--- app/src/main/java/com/example/myself/stuttersupport/StreakDbHelper.java
+++ app/src/main/java/com/example/myself/stuttersupport/StreakDbHelper.java
@@ -24,6 +24,8 @@ public class StreakDbHelper extends SQLiteOpenHelper {
     public static final String TABLE = "streak";
     public static final String C_TYPE = "type";
     public static final String C_VALUE = "value";
+    public static final String R_CURRENT = "current";
+    public static final String R_BEST = "best";
     Context context;
 
     public StreakDbHelper(Context context) {
@@ -37,10 +39,28 @@ public class StreakDbHelper extends SQLiteOpenHelper {
      */
     @Override
     public void onCreate(SQLiteDatabase db) {
+        //Create table
         String sql = "CREATE TABLE " + TABLE + " (" + C_TYPE + " TEXT PRIMARY KEY, " + C_VALUE +
         " TEXT)";
         db.execSQL(sql);
         Log.d("StreakDbHelper", "onCreate w sql: " + sql);
+
+        //Add two rows
+        ContentValues currentStreak = new ContentValues();
+        ContentValues bestStreak = new ContentValues();
+        currentStreak.clear();
+        bestStreak.clear();
+        currentStreak.put(C_TYPE, R_CURRENT);
+        currentStreak.put(C_VALUE, 0);
+        bestStreak.put(C_TYPE, R_BEST);
+        bestStreak.put(C_VALUE, 0);
+        try {
+            db.insertOrThrow(TABLE, null, currentStreak);
+            db.insertOrThrow(TABLE, null, bestStreak);
+        } catch (SQLException e){
+            Log.e("DATABASE", "ERROR when adding to DB");
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -54,5 +74,29 @@ public class StreakDbHelper extends SQLiteOpenHelper {
         //TODO: Temporary and probably bad
         db.execSQL("DROP TABLE IF EXISTS " + TABLE);
         onCreate(db);
+    }
+
+    public void updateCurrent(int newValue){
+
+    }
+
+    public void incrementBest(){
+        int oldBest;
+        ContentValues newRow = new ContentValues();
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.query(TABLE,
+                new String[]{C_VALUE},
+                C_TYPE+"=?",
+                new String[]{R_BEST},
+                null, null, null);
+        cursor.moveToFirst();
+
+        oldBest = cursor.getInt(1);
+        newRow.put(C_TYPE, R_BEST);
+        newRow.put(C_VALUE, oldBest++);
+
+        db.close();
+        db = getWritableDatabase();
+        db.update(TABLE, newRow, C_TYPE+"=?", new String[]{R_BEST});
     }
 }
