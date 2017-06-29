7192e88752eb412ffad803357a7cdb332e4b9ff0
Alexis Varsava <av11sl@brocku.ca>
2017-06-29 12:19:37 -0400

    Prompts user to send a message on milestone
    Compares current streak value to set of milestones, or for larger
    values, checks if streak is a multiple of 50. If true, prompts user to
    share their accomplishments. Intent is generic to allow using any social
    media app installed on the device, or can be sent to SMS or own calendar
    app.
    
    May alter to be specific to Facebook/Twitter later.

--- app/src/main/java/com/example/myself/stuttersupport/MainMenuActivity.java
+++ app/src/main/java/com/example/myself/stuttersupport/MainMenuActivity.java
@@ -1,11 +1,9 @@
 package com.example.myself.stuttersupport;
 
-import android.content.ContentValues;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.SQLException;
-import android.database.sqlite.SQLiteDatabase;
-import android.icu.text.SimpleDateFormat;
-import android.icu.util.Calendar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -13,16 +11,16 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import java.util.ArrayList;
 
-import java.util.Date;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Locale;
 
 public class MainMenuActivity extends FragmentActivity {
+    private final List<Integer> MILESTONES = new LinkedList<>(Arrays.asList(1, 7, 31, 50, 75, 100));
     /**
      * Handles animation, allows swiping
      */
@@ -84,13 +82,56 @@ public class MainMenuActivity extends FragmentActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data){
         if(resultCode == RESULT_OK){
+            int currentStreak = streakDbHelper.getCurrent();
             trackerDbHelper.addDateToDb(streakDbHelper);
             trackerPage.refreshCalendar(trackerDbHelper);
             trackerPage.refreshStreak(trackerDbHelper, streakDbHelper);
+            if(MILESTONES.contains(currentStreak) || isLargeMilestone(currentStreak)) {
+                showDialog(this, "Congratulations!", "You've hit a new milestone! " +
+                        "Want to share it with the world?");
+            }
         }
     }
 
+    private boolean isLargeMilestone(int currentStreak) {
+        return (currentStreak > 100) && (currentStreak % 50 == 0);
+    }
 
+    /**
+     * based on https://stackoverflow.com/questions/8227820/alert-dialog-two-buttons
+     * @param activity
+     * @param title
+     * @param message
+     */
+    public void showDialog(Activity activity, String title, CharSequence message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        DialogInterface.OnClickListener positiveListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                createSocialIntent();
+            }
+        };
+
+        if (title != null) builder.setTitle(title);
+
+        builder.setMessage(message);
+        builder.setPositiveButton("OK", positiveListener);
+        builder.setNegativeButton("Cancel", null);
+        builder.show();
+    }
+
+    private void createSocialIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, generateShareMessage());
+        startActivity(Intent.createChooser(shareIntent, "Share..."));
+    }
+
+    private String generateShareMessage() {
+        int streak = streakDbHelper.getCurrent();
+        if(streak == 1) return "I just started practicing my speech using Stutter Support!";
+        return "I've practiced my speech for " + streak + " days using Stutter Support!";
+    }
 
     /**
      * Pager adapter that represents 5 SettingsScreenFragment objects,
