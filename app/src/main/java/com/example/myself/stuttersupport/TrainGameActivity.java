5999172495db8aa3f8b625c2643d48e695a843e7
Alexis Varsava <av11sl@brocku.ca>
2017-07-10 16:51:29 -0400

    Converts train_game_pairs to array, adds getPair
    Converting to string array means I can get the entire array at once and
    then choose a specific entry in it, which makes selecting a random pair
    much easier.
    
    Issues:
    -Probably store the result of getResources for time's sake later on

--- app/src/main/java/com/example/myself/stuttersupport/TrainGameActivity.java
+++ app/src/main/java/com/example/myself/stuttersupport/TrainGameActivity.java
@@ -4,10 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class TrainGameActivity extends AppCompatActivity {
+    private final int MIN_PAIR = 1;
+    private final int MAX_PAIR = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_train_game);
+    }
+
+    private String[] getPair(){
+        String[] pair = new String[2];
+        int pairId = Numbers.randomInt(MIN_PAIR, MAX_PAIR);
+
+        pair[0] = getResources().getStringArray(R.array.calls)[pairId];
+        pair[1] = getResources().getStringArray(R.array.resps)[pairId];
+
+        return pair;
     }
 }
