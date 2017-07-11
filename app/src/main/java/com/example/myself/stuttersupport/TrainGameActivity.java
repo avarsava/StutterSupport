2b6515becbb97d6de471152a74c6be8a619ac389
Alexis Varsava <av11sl@brocku.ca>
2017-07-11 11:33:17 -0400

    Sets up TrainGameActivity, adds rounds preference
    TrainGameActivity is ready for development, I think.
    
    TODO:
    - Lots of repeated code between DeepBreatheActivity and
    TrainGameActivity. When I'm done I'll see how much is still shared
    between them and probably refactor.

--- app/src/main/java/com/example/myself/stuttersupport/TrainGameActivity.java
+++ app/src/main/java/com/example/myself/stuttersupport/TrainGameActivity.java
@@ -1,16 +1,54 @@
 package com.example.myself.stuttersupport;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class TrainGameActivity extends AppCompatActivity {
     private final int MIN_PAIR = 1;
     private final int MAX_PAIR = 1;
+    private enum STATE {CALL, WAIT, RESP};
+
+    private SharedPreferences prefs;
+    private DrawView screen;
+    private int maxCycles;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_train_game);
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        maxCycles = Integer.valueOf(prefs.getString("noOfPairs", "1"));
+        screen = new TrainGameView(this);
+        setContentView(screen);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        screen.resume();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        screen.pause();
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        screen.pause();
+        screen = null;
+    }
+
+    protected void killIfCountHigh(int cycleCount) {
+        if(cycleCount >= maxCycles) {
+            setResult(RESULT_OK);
+            finish();
+        }
     }
 
     private String[] getPair(){
@@ -21,5 +59,15 @@ public class TrainGameActivity extends AppCompatActivity {
         pair[1] = getResources().getStringArray(R.array.resps)[pairId];
 
         return pair;
+    }
+
+    private class TrainGameView extends DrawView {
+        public TrainGameView(Context context) {
+            super(context);
+        }
+
+        protected void doDrawing(){
+            
+        }
     }
 }
