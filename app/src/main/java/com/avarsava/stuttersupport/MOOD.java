1708b40b3b53a6edda3903aab359ffc7c965ad56
Alexis Varsava <av11sl@brocku.ca>
2018-02-28 20:28:58 -0500

    Adds MOOD enum for use with Thought Tracker

--- app/src/main/java/com/avarsava/stuttersupport/MOOD.java
+++ app/src/main/java/com/avarsava/stuttersupport/MOOD.java
@@ -1,3 +1,5 @@
+package com.avarsava.stuttersupport;
+
 /**
  * @author  Alexis Varsava <av11sl@brocku.ca>
  * @version 1.1
@@ -7,4 +9,9 @@
  */
 
 public enum MOOD {
+    //Moods
+    HAPPY, SAD, ANGRY, TIRED, EMBARRASSED, EXCITED, NERVOUS,
+
+    //Rating of one to ten
+    ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN
 }
