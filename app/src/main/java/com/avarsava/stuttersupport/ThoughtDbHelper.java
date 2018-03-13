39c05e8783c6651f183b51aca834ad65c176593c
Alexis Varsava <av11sl@brocku.ca>
2018-03-13 09:47:48 -0400

    Finishes DB Logic

--- app/src/main/java/com/avarsava/stuttersupport/ThoughtDbHelper.java
+++ app/src/main/java/com/avarsava/stuttersupport/ThoughtDbHelper.java
@@ -6,12 +6,24 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 /**
  * @author  Alexis Varsava <av11sl@brocku.ca>
@@ -85,14 +97,19 @@ public class ThoughtDbHelper extends DatabaseHelper {
     }
 
     public DBEntry[] getTodaysThoughts(){
+        return getThoughtsOnDate(DbDate.getDateString());
+    }
+
+    public DBEntry[] getThoughtsOnDate(String dateString){
         DBEntry[] list;
-        String dateString = DbDate.getDateString();
         String sql = "SELECT * FROM "
                 + TABLE +
                 " WHERE "
                 + C_DATE +
                 " = \""
-                + dateString +"\";";
+                + dateString +"\""
+                + " ORDER BY "
+                + C_DATE + " ASC;";
         String thought, mood;
         SQLiteDatabase db = getReadableDatabase();
         Cursor cursor = db.rawQuery(sql, null);
@@ -112,19 +129,89 @@ public class ThoughtDbHelper extends DatabaseHelper {
         return list;
     }
 
-    //TODO: Implement me!
-    public DBEntry[] mostCommonThoughts(){
+    public List<String> mostCommonThoughts(){
+        //Set up data
+        List<String> sortedThoughts = new ArrayList<>();
 
+        String currentThought;
+        Map<String, Integer> thoughtList = new HashMap<>();
+        List<DBCounter> dbCounterList = new ArrayList<>();
+        String sql = "SELECT "
+                + C_THOUGHT +
+                " FROM "
+                + TABLE + ";";
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery(sql, null);
+        cursor.moveToFirst();
+
+        //Count all the strings
+        while(cursor.moveToNext()) {
+            currentThought = cursor.getString(0);
+
+            if (thoughtList.containsKey(currentThought)) {
+                thoughtList.put(currentThought, ((Integer) thoughtList.get(currentThought)) + 1);
+            } else {
+                thoughtList.put(currentThought, 0);
+            }
+        }
+
+        db.close();
+
+        for(Map.Entry<String, Integer> e : thoughtList.entrySet()){
+            dbCounterList.add(new DBCounter(e.getKey(), e.getValue()));
+        }
+
+        //Sort by count
+        Collections.sort(dbCounterList, new Comparator<DBCounter>() {
+            @Override
+            public int compare(DBCounter o1, DBCounter o2) {
+                return o1.getCount() - o2.getCount();
+            }
+        });
+
+        for(DBCounter d : dbCounterList){
+            sortedThoughts.add(d.getThought());
+        }
+
+        return sortedThoughts;
     }
 
-    //TODO: Implement me!
-    public DBEntry[] lastThirtyDaysThoughts(){
 
+    public List<DBEntry> lastThirtyDaysThoughts(){
+        List<DBEntry> sortedList = new ArrayList<>();
+
+        for(int offset = 0; offset <= 30; offset++){
+            Date currentDate = new Date();
+            currentDate.setDate(currentDate.getDate()-offset);
+            List<DBEntry> newList = Arrays.asList
+                    (getThoughtsOnDate(DbDate.getDateString(currentDate)));
+            sortedList.addAll(newList);
+        }
+
+        return sortedList;
     }
 
-    //TODO: Implement me!
     public int averageMoodOnDate(String dateString){
+        String sql = "SELECT "
+                + C_MOOD +
+                " FROM "
+                + TABLE +
+                " WHERE "
+                + C_DATE +
+                " = \""
+                + dateString + "\";";
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery(sql, null);
+        cursor.moveToFirst();
 
+        int count = cursor.getCount();
+        int sum = 0;
+
+        while(cursor.moveToNext()){
+            sum += cursor.getInt(0);
+        }
+
+        return sum / count;
     }
 
     public class DBEntry{
@@ -142,6 +229,25 @@ public class ThoughtDbHelper extends DatabaseHelper {
 
         public String getMood(){
             return mood;
+        }
+
+    }
+
+    public class DBCounter{
+        String thought;
+        Integer count;
+
+        public DBCounter(String t, Integer i){
+            this.thought = t;
+            this.count = i;
+        }
+
+        public String getThought(){
+            return thought;
+        }
+
+        public Integer getCount(){
+            return count;
         }
     }
 }
